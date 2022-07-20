686f46f192c1e494936a26355805b1d9adb41ac6
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-20 17:42:08 -0500

    Adding Droppable Area & setting up handler

--- src/app.ts
+++ src/app.ts
@@ -176,7 +176,8 @@ class ProjectItem
   }
   @autoBind
   dragStartHandler(event: DragEvent): void {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
   }
   dragEndHandler(_: DragEvent): void {
     console.log("DragEnd!");
@@ -206,11 +207,18 @@ class ProjectList
   }
   //   Public
   @autoBind
-  dragOverHandler(_: DragEvent): void {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent): void {
+    if (event.dataTransfer && event.dataTransfer.types[0] == "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
-  dropHandler(_: DragEvent): void {}
+
+  dropHandler(event: DragEvent): void {
+    console.log(event);
+  }
+
   @autoBind
   dragLeaveHandler(_: DragEvent): void {
     const listEl = this.element.querySelector("ul")!;
