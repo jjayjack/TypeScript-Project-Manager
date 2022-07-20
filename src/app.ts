fd3b5109f62d02e1e47176cac368c849530146d6
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-20 11:43:49 -0500

    Added method to grab user data, validate that all inputs are filled and clears input boxes after submit

--- src/app.ts
+++ src/app.ts
@@ -45,10 +45,37 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
+  private getUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, please try again");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+  //   empty inputs after submit
+  private clearInput() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
   @autoBind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.getUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log([title, desc, people]);
+      this.clearInput();
+    }
   }
   private configure() {
     this.element.addEventListener("submit", this.submitHandler);
