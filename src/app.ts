2c2e2287008fa7b5059bc4b5c970e5ff425d13fc
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-20 11:17:47 -0500

    Target HTML elements to collect user input

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
       document.getElementById("project-input")!
@@ -12,8 +15,29 @@ class ProjectInput {
       this.templateElement.content,
       true
     );
+    // this.element = form element
     this.element = importedHTMLContent.firstElementChild as HTMLFormElement;
+    // Adding CSS to element created
+    this.element.id = "user-input";
+    // Get access to inputs
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+    this.configure();
     this.attach();
+  }
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
