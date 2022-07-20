92bfe63c2f7effde7ef4fcb4c5f0398a43393c8b
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-20 17:54:01 -0500

    Adding dropping capability

--- src/app.ts
+++ src/app.ts
@@ -56,6 +56,18 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active
     );
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+
+  //   move project
+  changeProjectState(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((project) => project.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -214,9 +226,13 @@ class ProjectList
       listEl.classList.add("droppable");
     }
   }
-
+  @autoBind
   dropHandler(event: DragEvent): void {
-    console.log(event);
+    const projectId = event.dataTransfer!.getData("text/plain");
+    projectState.changeProjectState(
+      projectId,
+      this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+    );
   }
 
   @autoBind
